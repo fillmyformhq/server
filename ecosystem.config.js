452bbc275e67fb537779678205913021f59315af
AdityaKG-169 <aditya.kg169@gmail.com>
2021-09-30 22:19:59 +0530

    changed ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
 	apps: [
 		{
 			name: "fillmyform-server-prod",
-			script: "npx",
-			args: "start",
+			script: "pm2",
+			args: "start npm -- start",
 			interpreter: "none",
 			env: {
 				NODE_ENV: "production",
