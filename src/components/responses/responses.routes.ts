5dee762002cb6f705de35387f8a7012087f36f43
AdityaKG-169 <aditya.kg169@gmail.com>
2021-10-11 02:15:22 +0530

    Features testing complete

--- src/components/responses/responses.routes.ts
+++ src/components/responses/responses.routes.ts
@@ -3,7 +3,6 @@ import checkOrigin from "../../middlewares/checkOrigin.middlewares";
 import ipRequestLimiter from "../../middlewares/ipRequestLimiter.middlewares";
 import verifyToken from "../../middlewares/verifyToken.middlewares";
 import {
-	getResponse,
 	listAllResponses,
 	saveResponse,
 } from "./responses.controllers";
@@ -12,7 +11,6 @@ const router: Router = Router();
 
 // make a response number limiter
 router.post("/:appId", ipRequestLimiter(60, 3), saveResponse); //save response
-router.get("/list/all", [checkOrigin, verifyToken], listAllResponses); // list responses
-router.get("/:id", [checkOrigin, verifyToken], getResponse); //get a response
+router.get("/list", [checkOrigin, verifyToken], listAllResponses); // list responses
 
 export default router;
